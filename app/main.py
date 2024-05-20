aff009fce41addbc677f00dd9595271212b5141d
unknown <johnguogan@outlook.com>
2024-05-20 12:08:49 -0300

    update identity-iq api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import time
 import os
+import json
 
+from scrapyd_api import ScrapydAPI
 from dotenv import load_dotenv
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
@@ -9,6 +11,8 @@ from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
 from app.IdentityIQ.spiders.ident_iq import IdentIqSpider
 from app.settings import get_settings
+
+scrapyd = ScrapydAPI('http://localhost:6800')
 
 load_dotenv()
 
@@ -48,26 +52,15 @@ async def predict(identity_iq_user: IdentityIqUser, bt: BackgroundTasks) -> str:
     print(identity_iq_user)
 
     current_time = time.time() * 1000
-    # output_file_path = f"s3://{AWS_S3_BUCKET_NAME}/credit-reports/{identity_iq_user.user_id}/IdentityIQ/{current_time}_report.json"
-    output_file_path = f"./scraper-output/{identity_iq_user.user_id}/IdentityIQ/{current_time}_report.json"
+    output_file_path = f"s3://{AWS_S3_BUCKET_NAME}/credit-reports/{identity_iq_user.user_id}/IdentityIQ/{current_time}_report.json"
+    # output_file_path = f"./scraper-output/{identity_iq_user.user_id}/IdentityIQ/{current_time}_report.json"
 
     feeds = { f'{output_file_path}': { 'format': 'json', 'encoding': 'utf8', 'store_empty': False, 'indent': 4, } }
+    settings = {
+        "FEEDS": json.dumps(feeds)
+    }
+    
+    job_id = scrapyd.schedule('IdentityIQ', 'ident_iq', settings = settings, user_id = identity_iq_user.user_id, http_user = identity_iq_user.username, http_pass = identity_iq_user.password, last_four_digits = identity_iq_user.last_four_digits)
 
-    # settings = get_project_settings()
-    settings = get_settings()
-    settings["FEEDS"] = feeds
-
-    # settings[ "FEEDS" ] = feeds
-    # settings.setdefault("FEEDS", { }).update(feeds)
-
-    print(settings)
-
-    process = CrawlerProcess(settings)
-    process.crawl(
-            IdentIqSpider, user_id = identity_iq_user.user_id, http_user = identity_iq_user.username,
-            http_pass = identity_iq_user.password, last_four_digits = identity_iq_user.last_four_digits
-    )
-    bt.add_task(process.start, stop_after_crawl = False)
-
-    print("I'm here.")
-    return "IDIQ Done!"
+    print(f'Scheduled job with ID: {job_id}')
+    return f'IDIQ Done! Scheduled job with ID: {job_id}'
